86f8290f0f617754c2798d674193a268e3bbb3e1
Neelam Tiwari <neelamtiwari411@gmail.com>
2020-12-18 10:11:21 +0530

    yet another commit

--- JAVA_Codility/src/com/ArrayCodility/TaskCodility1.java
+++ JAVA_Codility/src/com/ArrayCodility/TaskCodility1.java
@@ -5,32 +5,7 @@ import java.util.Scanner;
 public class TaskCodility1 {
 	public static void rotate(int[] arr,int n,int r)
 	{
-	/*	if(r<n)
-		{
-			System.out.println("array length");
-		}
-		int[] temp=new int[r+1];
-		int j=0,r1=r,i;
-		for(i=0;i<arr.length;i++)
-		{
-			System.out.println("check "+i);
-			temp[j]=arr[i];
-			if(r1==0)
-				break;
-			arr[i] = arr[n-r1];
-			r1--;
-			j++;
-		}
-		
-		for(j=0;i<n;i++)
-		{
-			if(j<=r-1)
-			{
-			arr[i]=temp[j];
-			j++;
-			}
-				
-		}*/
+	
 		int j;
 		int[] newArr =new int[arr.length];
 		for(int i=0;i<arr.length;i++)
